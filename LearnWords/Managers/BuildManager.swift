7fee790fa62f37f17df0da8b34a896f963798d64
SergeiKrainov <89527271+SergeiKrainov@users.noreply.github.com>
2024-03-05 13:19:37 +0300

    add OSSSpeechKit pod

---
+++ LearnWords/Managers/BuildManager.swift
@@ -0,0 +1,8 @@
+//
+//  BuildManager.swift
+//  LearnWords
+//
+//  Created by Сергей Крайнов on 04.03.2024.
+//
+
+import Foundation
---
+++ Pods/Target Support Files/OSSSpeechKit/OSSSpeechKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_OSSSpeechKit : NSObject
+@end
+@implementation PodsDummy_OSSSpeechKit
+@end
